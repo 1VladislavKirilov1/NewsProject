894ea9cd8b185ced926589b015b0c1421e2668a1
1VladislavKirilov1 <vlad.kirilov1050@gmail.com>
2023-05-21 17:04:09 +0300

    Update project

--- News/NewsPaper/NewsPaper/settings.py
+++ News/NewsPaper/NewsPaper/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'news',
     'NewsPaper',
     'fpages',
+    'django_filters',
 ]
 
 SITE_ID = 1
--- News/NewsPaper/news/views.py
+++ News/NewsPaper/news/views.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from django.views.generic import ListView, DetailView
 from .models import Post
+from .filters import PostFilter
 
 
 class PostList(ListView):
@@ -8,6 +9,7 @@ class PostList(ListView):
     ordering = 'title'
     template_name = 'posts.html'
     context_object_name = 'posts'
+    paginate_by = 10
 
 
 class PostDetail(DetailView):
@@ -18,6 +20,23 @@ class PostDetail(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['time_now'] = datetime.utcnow()
+        return context
+
+
+class PostSearch(ListView):
+    form_class = PostFilter
+    model = Post
+    template_name = 'search.html'
+    context_object_name = 'search'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        self.filterset = PostFilter(self.request.GET, queryset)
+        return self.filterset.qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['filterset'] = self.filterset
         return context
 
 
